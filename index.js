ee3bfc5731bbb9fe9860dc50164ddc8ac9529701
pmc0707 <pranshugoodboy@gmail.com>
2024-08-15 20:42:17 +0530

    (ROUTING)

--- index.js
+++ index.js
@@ -7,14 +7,30 @@ app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
 
-app.use(( req,res)=>{
+/*app.use(( req,res)=>{
     console.log( "request received" );
-    // res.send({
-    //     name :"apple",
-    //     color:"red"
-    //
-    // })
+    res.send({
+        name :"apple",
+        color:"red"
+
+    })
     let code = "<h1>Hey! this is pmc0707 world</h1>";
     res.send(code);
 
+});*/
+app.get("/", (req, res) => {
+    res.send("<h1>Hey! you reach pmc0707 world</h1>");
+})
+app.get("/apple", (req, res) => {
+    res.send("you reach apple page");
+})
+app.get("/banana", (req, res) => {
+    res.send("you reach banana page");
+})
+app.get("/mango", (req, res) => {
+    res.send("you reach mango page");
+})
+
+app.get("*", (req, res) => {
+    res.send("<h1>ERROR! *_*</h1>");
 })
