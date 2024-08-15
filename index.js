8619667c518e162061ac5478572b7993410fb864
pmc0707 <pranshugoodboy@gmail.com>
2024-08-15 20:14:50 +0530

    sending of response to the sever

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ let port = 8080; //3000
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
+
+app.use(( req,res)=>{
+    console.log( "request received" );
+    res.send({
+        name :"apple",
+        color:"red"
+
+    })
+})
